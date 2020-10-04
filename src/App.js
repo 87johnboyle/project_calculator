1baad2f7d4158f77b638a56953ad76f605434eaa
87johnboyle <john.hazukisan@gmail.com>
2020-10-04 20:08:15 +0100

    working, no css

--- src/App.js
+++ src/App.js
@@ -12,13 +12,55 @@ class App extends Component {
     }
   }
 
+  onClick = button => {
+    if(button === "=") {
+      this.calculate()
+    }
+
+    else if(button === "C"){
+      this.reset()
+    }
+    else if(button === "CE"){
+      this.backspace()
+    }
+    else {
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+  };
+
+  calculate = () => {
+    try {
+      this.setState({
+        result:(eval(this.state.result) || "" ) + ""
+      })
+    } catch (e) {
+      this.setState({
+        result: "error"
+      })
+    }
+  };
+
+reset = () => {
+  this.setState({
+    result: ""
+  })
+};
+
+backspace = () => {
+  this.setState ({
+    result: this.state.result.slice(0, -1)
+  })
+};
+
 render() {
   return (
     <div>
       <div className="calculator-body">
         <h1> Calculator </h1>
         <ResultComponent result={this.state.result}/>
-        <KeyPadCompnent onClick={this.onClick}/>
+        <KeyPadComponent onClick={this.onClick}/>
       </div>
     </div>
     );
